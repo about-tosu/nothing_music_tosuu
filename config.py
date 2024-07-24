63bf8d3a0f203daab653cd31f6aef26b84f74363
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-07-24 20:30:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ from pyrogram import filters
 
 load_dotenv()
 
-API_ID = 
-API_HASH = ""
-BOT_TOKEN = ""
+API_ID = 24620300
+API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
+BOT_TOKEN = "6915408680:AAGcxq-eVClT3x8Kd2aNlrS0m0sCjnXjDgw"
 MONGO_DB_URI = ""
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
-LOG_GROUP_ID = 
-OWNER_ID = 
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999))
+LOG_GROUP_ID = -1002023182491
+OWNER_ID = 6848223695
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
