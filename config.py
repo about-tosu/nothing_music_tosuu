8fe3a16962ade17bc1eac48a82691733697ee5c3
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-08-25 01:10:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 API_ID = 24620300
 API_HASH = "9a098f01aa56c836f2e34aee4b7ef963"
-BOT_TOKEN = "6457568929:AAEjV58bTPE-ioYY6TFX4VvuPOlXOHY7ytc"
-MONGO_DB_URI = "mongodb+srv://Zenitsuaf:Zenitsuaf@cluster0.i58aapw.mongodb.net/?retryWrites=true&w=majority"
+BOT_TOKEN = "6915408680:AAH5XRPWZfCldG_KHiiFNMczvB1PrzktvKA"
+MONGO_DB_URI = "mongodb+srv://fidixi3663:w7rvlxmDd5lsX9ix@cluster0.0k1an50.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 9999))
-LOG_GROUP_ID = -1002061338810
-OWNER_ID = 5322882174
+LOG_GROUP_ID = -1002023182491
+OWNER_ID = 6848223695
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -52,8 +52,8 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 
 # Get your pyrogram v2 session from Replit
-STRING1 = "BQFOkGcAPawKkEv68pS9YR8guk6dCqzWCi-9EhOl8l5sL6APIhFk9sAi0jL21bwuf73ZzWS1UmjDxcztFh_O3TF8j99N6BrSjZDr38ZN9b9jUo9BCATUb0-1rmOe_nKETLqUMXmUh5M4Bdok2yd3fX7jlJE4upMJJ4xDguQH3UvXLJk1QmO7M2HCWmvrganrN3PLmSYJhgeEwFU-QBygU57sTTdfpzW_OVpXogP5z6UkN7nfDzq8LdsAc7GUP4HRq1BOIswS03hVJQ0ogxPFnkZJH4xg5iAoK7RUsRbIqbd1tqEC5eF1vxi9tpul2JQC2EUBznFCziyfdk18uSt1w1LVFGjO7QAAAAGXQ2Y9AA"
-STRING2 = ""
+STRING1 = "BQF3rQwAKBrsDELpOEfY6YDzNfTJ3Y101Gk_gr5ZQOYG6Xh6Gc5M52r8uS7LnyCmX0iq7HkWrbLlS9UPFUv7LCElBfaATiP4eEZ-tQehr1A8vo7XufLbsvppJIEP5xDODaKUauZ_2Z85d8A2VTiCLC6EDM_esRqjzQIvQXE9VHq-Plntsxjcb_K5dp51wI3vBBfk-ZIq2J0fmixamkE0sziMB4s3s0r1vYVlbDGrbecPt70cavgneN_Xo7WAHtOUyBSCrq87nC1RIxzuE09IYhdE-gr8Ix_bG7pXygelRqKu-fgnFcfEInphOsy-nSKN_mg0aliEJOup2Ux-4uUXvuYZnhUnygAAAAE1mGFGAA"
+STRING2 = "BQF3rQwAZrQtzPqDkma6TUD5QxUjzGq5hAaeDH2nY_eN-D38eBIFFJH7Ha76eSQtLNvxMUrRrFOKKar6RjEfr_xoaBBmO_j2VbzMhw4sI1Z03lIGYU-OjekptdPgantGeMW9Qn7DmoHhsKJpUNe_KwlGGD3SM1qD1v0ZGr-1fSZtg9IxAX0JmcncNZWinG57mFizkPGuPYr2jl9MleHH6XL6yXAdNDiYplaOHD8trZKQCgBkVgr_ABajcim-mXpd0g82XFtb5Cv8kF7kF-e0rgbG4XealK2cMC83JD4G3-8eWfYbWsRg01_fkHFq5UAA_UJN6BX0YBXS8GUpE9nB0lel_z2RtgAAAAGhjpphAA"
 STRING3 = ""
 STRING4 = getenv("STRING_SESSION4", None)
 STRING5 = getenv("STRING_SESSION5", None)
